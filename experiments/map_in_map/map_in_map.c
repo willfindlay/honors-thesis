f46cd81e55a2eca35cda1773e1174d58e632eca7
HousedHorse <william@wfindlay.com>
2019-09-03 20:38:52 -0400

    save experiments before heading to school

--- experiments/map_in_map/map_in_map.c
+++ experiments/map_in_map/map_in_map.c
@@ -1,19 +1,45 @@
 BPF_TABLE_PINNED("hash", u64, u64, map_in_map, 10480, "/sys/fs/bpf/map_in_map");
 
+BPF_PERF_OUTPUT(testificate);
+
+struct test {
+    u64 test;
+};
+
 TRACEPOINT_PROBE(raw_syscalls, sys_enter)
 {
+    struct test test = {0};
+    u64 key = 12;
+    u64 test_num = 2456;
+    u64 *testp;
     u64 syscall = args->id;
-    u64 *inner_fd = NULL;
-    inner_fd = map_in_map.lookup(&syscall);
 
-    u64 new_fd = bpf_create_map((enum bpf_map_type)BPF_MAP_TYPE_HASH, sizeof(u64), sizeof(u64), 10240, 0);
+    BPF_FUNC_probe_read;
 
-    //if (inner_fd == NULL)
-    //{
-    //    //int outer_id =
-    //    //bpf_map_update_elem(map_in_map, &syscall, inner_fd, 0);
-    //    //map_in_map.update(&syscall, fd);
-    //}
+    void *inner_map;
+
+    /* sys_getuid */
+    if (syscall != 102)
+        return 0;
+
+    inner_map = map_in_map.lookup(&key);
+
+    if (inner_map)
+    {
+        testp = bpf_map_lookup_elem(inner_map, &key);
+
+        if (testp == NULL)
+        {
+            bpf_map_update_elem(inner_map, &key, &syscall, BPF_ANY);
+        }
+
+        testp = bpf_map_lookup_elem(inner_map, &key);
+
+        if (testp != NULL)
+            test.test = *testp;
+
+        testificate.perf_submit(args, &test, sizeof(test));
+    }
 
     return 0;
 }
