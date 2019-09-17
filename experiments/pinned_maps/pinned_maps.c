8bc48d75422d67b130399ef6b1309ece44a3f1a5
HousedHorse <william@wfindlay.com>
2019-09-17 12:11:57 -0400

    pinned map saving proof of concept

--- experiments/pinned_maps/pinned_maps.c
+++ experiments/pinned_maps/pinned_maps.c
@@ -1,1 +1,11 @@
-BPF_HASH(testificate, u64, u32);
+BPF_TABLE_PINNED("hash", u64, u32, testificate, 10240, "/sys/fs/bpf/testificate");
+
+TRACEPOINT_PROBE(raw_syscalls, sys_enter)
+{
+    u64 pid = bpf_get_current_pid_tgid();
+    u32 testval = 3;
+
+    testificate.update(&pid, &testval);
+
+    return 0;
+}
