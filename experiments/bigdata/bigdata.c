8e868924519a38eb5de36821439e7e3211f63189
HousedHorse <william@wfindlay.com>
2019-10-04 09:58:12 -0400

    touch proposal

--- experiments/bigdata/bigdata.c
+++ experiments/bigdata/bigdata.c
@@ -1,17 +1,28 @@
 typedef struct
 {
-    u8 test[300 * 300];
+    u64 key;
+    u8 test[32000];
 }
 big_data;
 
 BPF_PERF_OUTPUT(event);
-BPF_PERCPU_ARRAY(testificate, big_data, 1);
+BPF_PERCPU_ARRAY(testificate, big_data, 3);
+BPF_ARRAY(curr_idx, int, )
 
 TRACEPOINT_PROBE(raw_syscalls, sys_enter)
 {
-    int key = 0;
+    int zero = 0;
+    int one  = 0;
+    int two  = 0;
 
-    big_data *d = testificate.lookup(&key);
+    big_data *d0 = testificate.lookup(&zero);
+    big_data *d1 = testificate.lookup(&one);
+    big_data *d2 = testificate.lookup(&two);
+
+    for (int i = 0; i < 32000; i++)
+    {
+
+    }
 
     return 0;
 }
--- experiments/memory-leak/memleak.py
+++ experiments/memory-leak/memleak.py
@@ -7,7 +7,7 @@ from bcc import BPF
 from bcc.libbcc import lib
 
 if __name__ == "__main__":
-    bpf = BPF(src_file="bigdata.c")
+    bpf = BPF(src_file="memleak.c")
 
     def on_event(cpu, data, size):
         event = bpf['event'].event(data)
@@ -19,4 +19,8 @@ if __name__ == "__main__":
             bpf.perf_buffer_poll(30)
             time.sleep(1)
         except KeyboardInterrupt:
+            bdm = bpf["big_data_map"].values()
+            for data in bdm:
+                with open(f'/tmp/testificate-{data.pid_tgid}', 'wb') as f:
+                    f.write(data)
             sys.exit(0)
