7b7393283c1b1a4c2830bc10e630d02965db758e
willfindlay <william@williamfindlay.com>
2020-01-15 08:33:05 -0500

    more experiments

---
+++ proposal/ebpH/scripts/install.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+DIR=$(dirname $(readlink -f $0))/..
+INSTALLDIR=/opt/ebpH
+
+cd $DIR
+mkdir -p $INSTALLDIR
+
+# make sure root owns /opt/ebpH
+chown root:root $INSTALLDIR
+
+# copy everything into /opt/ebpH
+cp -r ./* $INSTALLDIR
+
+# copy systemd unit file
+cp ./systemd/ebphd.service /etc/systemd/system/ebphd.service
+
+# navigate to /opt/ebpH
+cd $INSTALLDIR
+
+# create the symbolic link for ebphd
+ln -sf $(readlink -f ./ebphd) /bin/ebphd
